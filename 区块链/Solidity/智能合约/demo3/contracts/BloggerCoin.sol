af0f83604479cf6071b71735cd3a9ade1a28ba8a
Ben <892156860@qq.com>
2018-08-08 10:39:41 +0800

    提交部落币

--- 区块链/Solidity/智能合约/demo3/contracts/BloggerCoin.sol
+++ 区块链/Solidity/智能合约/demo3/contracts/BloggerCoin.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.4.4;
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract BloggerCoin is StandardToken {
 
-    string public name = "珏豪币";
+    string public name = "BloggerCoin";
     string public symbol = "BLC";
     uint8 public decimals = 4;
     uint256 public INITIAL_SUPPLY = 666666;
 
-    constructor () {
-        totalSupply = INITIAL_SUPPLY;
+    constructor () public {
+        totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
 
